fd7d4464c86366b5c6b0fffb76e5a45396257c8d
Luís Cruz <luiscruzdesign@gmail.com>
2026-07-29 20:39:50 -0300

    chore: configure code quality tooling

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,15 @@
 import { defineConfig, globalIgnores } from "eslint/config";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+
+  globalIgnores([".next/**", "out/**", "build/**", "coverage/**", "next-env.d.ts"]),
+
+  eslintConfigPrettier,
 ]);
 
 export default eslintConfig;
